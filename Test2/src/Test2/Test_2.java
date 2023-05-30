96e453a4c824d0eb6bbcc1d201fcde840a80bc9b
lhs1100 <129822594+lhs1100@users.noreply.github.com>
2023-05-30 17:20:48 +0900

    Add files via upload

---
+++ Test2/src/Test2/Test_2.java
@@ -0,0 +1,36 @@
+
+package Test2;
+
+abstract class Calculator {
+	public abstract int add(int a, int b);
+	public abstract int subtract(int a, int b);
+	public abstract double average(int[]a);
+}
+
+class GoodCalc extends Calculator {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public double average(int[]a) {
+		double sum=0;
+		for (int i=0;i<a.length; i++)
+			sum+=a[i];
+		return sum/a.length;
+	}
+}
+public class Test_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GoodCalc c=new GoodCalc();
+		System.out.println(c.add(2,  3));
+		System.out.println(c.subtract(2, 3));
+		System.out.println(c.average(new int[] {2,3,4}));
+	}
+
+}
